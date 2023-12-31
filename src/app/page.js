560bca599d9771624a6848b01a231b626d4776f6
Pritesh55 <iampritesh13@gmail.com>
2023-12-31 08:31:06 +0530

    Revert "Merge pull request #2 from Pritesh55/Product_added"

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,6 @@
 import Image from 'next/image'
 
-export default async function Home() {
-
-  const supabase = createServerComponentClient({ cookies });
-  const { data: tweets } = await supabase.from("tweets").select();
-  // wait for data from supabase's "tweets" table...
-
-  
-
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
